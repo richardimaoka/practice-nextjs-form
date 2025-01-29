a87db88ab44acba97d78ee4f9cf5732d3738e116
richardimaoka <richard.s.imaoka@gmail.com>
2025-01-30 03:51:20 +0900

    styling

--- app/ui/Search.tsx
+++ app/ui/Search.tsx
@@ -1,8 +1,9 @@
 import Form from "next/form";
+import styles from "./Search.module.css";
 
 export function Search() {
   return (
-    <Form action="/search">
+    <Form action="/search" className={styles.component}>
       {/* On submission, the input value will be appended to 
           the URL, e.g. /search?query=abc */}
       <input name="query" />

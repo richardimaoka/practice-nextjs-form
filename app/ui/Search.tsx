1618fb630252e8c23e4ec605f9ad61f97087f8a1
richardimaoka <richard.s.imaoka@gmail.com>
2025-01-30 04:02:40 +0900

    popop

--- app/ui/Search.tsx
+++ app/ui/Search.tsx
@@ -2,6 +2,10 @@ import Form from "next/form";
 import styles from "./Search.module.css";
 
 export function Search() {
+  function poppop() {
+    console.log("pppo");
+  }
+
   return (
     <Form action="/search" className={styles.component}>
       {/* On submission, the input value will be appended to 
